01a1f9c27e4eb597b3989050da21bb1f869c38ff
Harvey <hw-li@foxmail.com>
2019-11-08 00:06:50 +0800

    add file stats

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var fs=require("fs")
 
 // initial filelist after html loaded
 window.onload=function(){
-    showList("/users/cquda/onedrive/");
+    showList("/users/cquda/onedrive/web/fman/");
 }
 
 // input dir show filelist on windows
@@ -14,16 +14,43 @@ function showList(dir){
         //show filelist in html
         var fileListTable=document.getElementsByClassName("inflist");
         var tabelLeft=fileListTable[1];
-        tabelLeft.innerHTML=arrayToHtmlTable(files);
+        tabelLeft.innerHTML=nameToHtmlTable(files);
+        sizeToHtml(dir,files);
         console.log(files)    
     });
 }
 
 // transform array to html table
-function arrayToHtmlTable(files){
+function nameToHtmlTable(files){
     var tableString="";
     for(let i=0; i<files.length;i++){
         tableString=tableString+"<tr>"+"<td class='name'>"+files[i]+"</td></tr>"
     }
     return tableString;
+}
+function sizeToHtml(dir,files){
+    var names=document.getElementsByClassName("name");
+    
+    for(let i=0; i<files.length; i++){
+        var filedir=dir+files[i];
+        fs.stat(filedir, function (err, stats) {
+            if(stats.isFile()){
+                filedate=stats["atime"].toString().slice(0,15);
+                names[i+1].insertAdjacentHTML('afterend',"<td class='size'>"+stats["size"]+"B</td><td class='date'>"+filedate+"</td>")
+                console.log(stats["size"]);
+            }
+         });
+        
+    }
+}
+function filesize(dir,filename){
+    var filedir=dir+filename
+    fs.stat(filedir, function (err, stats) {
+        if (err) {
+            return console.error(err);
+        }
+        if(stats.isFile()){
+            console.log(stats["size"]);
+        }
+     });
 }
--- test/test.js
+++ test/test.js
@@ -1,14 +1,25 @@
 var fs = require("fs");
 
 // filelist=[];
-fs.readdir("/",function(err, files){
+// fs.readdir("/",function(err, files){
+   // if (err) {
+       // return console.error(err);
+   // }
+   // filelist=files;
+// });
+// fs.stat("\Users\cquda\OneDrive\Web\fman\index.js",function(err,stats){
+	// console.log(stats);
+	
+// })
+var k;
+fs.stat('/Users/cquda/OneDrive/Web/fman/main.js', function (err, stats) {
    if (err) {
        return console.error(err);
    }
-   // files.forEach( function (file){
-       // console.log( file );
-   // });
-   filelist=files;
-   // console.log( files);
+   console.log(stats["atime"].toString().slice(0,10));
+   console.log("读取文件信息成功！");
+   
+   // 检测文件类型
+   console.log("是否为文件(isFile) ? " + stats.isFile());
+   console.log("是否为目录(isDirectory) ? " + stats.isDirectory());    
 });
-console.log(filelist)
