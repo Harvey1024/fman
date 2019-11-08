7ac24f88de62b93130ee0cf7454dc635dd7ef7e4
Harvey <hw-li@foxmail.com>
2019-11-08 23:04:28 +0800

    Open folder model done.

--- index.js
+++ index.js
@@ -1,50 +1,99 @@
 var fs=require("fs")
 
+var dirnow;
 // initial filelist after html loaded
 window.onload=function(){
-    showList("/users/cquda/onedrive/web/fman/");
+    dirnow="C:/Users/cquda/OneDrive/"
+    showList(dirnow);
+}
+var sig=0;
+var EventEmitter = require('events').EventEmitter; 
+var event = new EventEmitter(); 
+
+// event.on('some_event', function() { 
+//     console.log('some_event 事件触发'); 
+//     fileclickfun();
+// }); 
+
+// setTimeout(function() { 
+//     event.emit('some_event'); 
+// }, 1500); 
+
+
+
+// file click function
+function fileclickfun(){
+    var filebtns=document.getElementsByClassName("name");
+    for(let i=0; i<filebtns.length;i++){
+      
+        filebtns[i].addEventListener('click', function(){
+            var btn=document.getElementById("leftfile"+i.toString());
+            if(btn.classList[1]=="folder"){
+                console.log("this is dir="+dirnow);
+                showList(dirnow+btn.innerText+"/");
+                
+                // fileclickfun();
+            }
+            else{
+                console.log("this is file "+btn.id);
+            }
+        })
+    }
+    console.log("click setted");
 }
 
 // input dir show filelist on windows
-function showList(dir){
-    fs.readdir(dir,function(err, files){
+function showList(directory){
+    dirnow=directory;
+    fs.readdir(directory,function(err, files){
         if (err) {
             return console.error(err);
         }
         //show filelist in html
         var fileListTable=document.getElementsByClassName("inflist");
         var tabelLeft=fileListTable[1];
-        tabelLeft.innerHTML=nameToHtmlTable(files);
-        sizeToHtml(dir,files);
-        console.log(files)    
-    });
+        tabelLeft.innerHTML=nameToHtmlTable(directory,files);
+        sizeToHtml(directory,files);
+
+        // fileclickfun() should be in call back function
+        fileclickfun();
+        dirnow=document.getElementById("leftdir").innerHTML;
+    });    
+    console.log("finished show list");
+    
 }
 
 // transform array to html table
-function nameToHtmlTable(files){
+function nameToHtmlTable(directory,files){
     var tableString="";
     for(let i=0; i<files.length;i++){
-        tableString=tableString+"<tr>"+"<td class='name'>"+files[i]+"</td></tr>"
+        var fileid="leftfile"+i.toString();
+        tableString=tableString+"<tr>"+"<td class='name',"+" id='"+ fileid+ "',name="+directory+">"+files[i]+"</td></tr>"
+        document.getElementById("leftdir").innerHTML=directory;
     }
     return tableString;
 }
-function sizeToHtml(dir,files){
+function sizeToHtml(directory,files){
     var names=document.getElementsByClassName("name");
     
     for(let i=0; i<files.length; i++){
-        var filedir=dir+files[i];
+        var filedir=directory+files[i];        
         fs.stat(filedir, function (err, stats) {
             if(stats.isFile()){
-                filedate=stats["atime"].toString().slice(0,15);
-                names[i+1].insertAdjacentHTML('afterend',"<td class='size'>"+stats["size"]+"B</td><td class='date'>"+filedate+"</td>")
-                console.log(stats["size"]);
+                var filedate=dateFormat(stats["atime"].toString().slice(4,21));
+                var filesize=fileSizeFormat(stats["size"]);
+                names[i].insertAdjacentHTML('afterend',"<td class='size'>"+filesize+"B</td><td class='date'>"+filedate+"</td>");
+                // console.log(stats["size"]);
+            }
+            if(stats.isDirectory()){
+                names[i].classList.add("folder");
             }
          });
         
     }
 }
-function filesize(dir,filename){
-    var filedir=dir+filename
+function filesize(directory,filename){
+    var filedir=directory+filename
     fs.stat(filedir, function (err, stats) {
         if (err) {
             return console.error(err);
@@ -53,4 +102,56 @@ function filesize(dir,filename){
             console.log(stats["size"]);
         }
      });
+}
+
+// transform date formate from "Thu Nov 07 2019 18:29:48 GMT+0" to "2019/11/07 18:29"
+function dateFormat(datestring){
+    var fileMonth=datestring.slice(0,3);
+    switch(fileMonth){
+        case "Jan":
+            fileMonth="01";
+            break;
+        case "Feb":
+            fileMonth="02";
+            break;
+        case "Mar":
+            fileMonth="03";
+            break;
+        case "Apr":
+            fileMonth="04";
+            break;
+        case "May":
+            fileMonth="05";
+            break;
+        case "Jun":
+            fileMonth="06";
+            break;
+        case "Jul":
+            fileMonth="07";
+            break;
+        case "Aug":
+            fileMonth="08";
+            break;
+        case "Sep":
+            fileMonth="09";
+            break;
+        case "Oct":
+            fileMonth="10";
+            break;
+        case "Nov":
+            fileMonth="11";
+            break;
+        case "Dec":
+            fileMonth="12";
+            break;
+    }
+    var fileDay=datestring.slice(4,6);
+    var fileYear=datestring.slice(7,11);
+    var fileHour=datestring.slice(12,17);
+    return fileYear+'/'+fileMonth+'/'+fileDay+' '+fileHour;
+}
+
+// transform date size from B to GB,M,KB
+function fileSizeFormat(size){
+    return size;
 }
--- test/test.js
+++ test/test.js
@@ -16,7 +16,51 @@ fs.stat('/Users/cquda/OneDrive/Web/fman/main.js', function (err, stats) {
    if (err) {
        return console.error(err);
    }
-   console.log(stats["atime"].toString().slice(0,10));
+   // console.log(stats["atime"].toString().slice(16,21));
+   var datestring=stats["atime"].toString().slice(4,21);
+    var fileMonth=datestring.slice(0,3);
+    switch(fileMonth){
+        case "Jan":
+            fileMonth="01";
+            break;
+        case "Fan":
+            fileMonth="02";
+            break;
+        case "Jan":
+            fileMonth="03";
+            break;
+        case "Mar":
+            fileMonth="04";
+            break;
+        case "Jan":
+            fileMonth="05";
+            break;
+        case "Apr":
+            fileMonth="06";
+            break;
+        case "May":
+            fileMonth="07";
+            break;
+        case "Jum":
+            fileMonth="08";
+            break;
+        case "Jul":
+            fileMonth="09";
+            break;
+        case "Aug":
+            fileMonth="10";
+            break;
+        case "Nov":
+            fileMonth="11";
+            break;
+        case "Dec":
+            fileMonth="12";
+            break;
+    }
+    var fileDay=datestring.slice(4,6);
+    var fileYear=datestring.slice(7,11);
+    var fileHour=datestring.slice(12,17);
+    console.log(fileYear+'/'+fileMonth+'/'+fileDay+' '+fileHour)
    console.log("读取文件信息成功！");
    
    // 检测文件类型
