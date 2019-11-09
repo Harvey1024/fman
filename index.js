88857673e9a06225b8af26e8887425d7344b24eb
Harvey <hw-li@foxmail.com>
2019-11-09 17:32:43 +0800

    add keypress function(up, down, enter)

--- index.js
+++ index.js
@@ -2,54 +2,148 @@ var fs=require("fs")
 var sys=require("sys")
 var exec = require('child_process').exec;
 
-var dirnow;
-var dirback;
+var dirnowL;
+var dirnowR;
+var activePan="left";
+var cursorPositionL=0;
+var cursorPositionR=0;
 // initial filelist after html loaded
 window.onload=function(){
-    dirnow="C:/Users/cquda/OneDrive/Web/fman/test/"    
-    showList(dirnow);
+    dirnowL="C:/Users/cquda/OneDrive/Web/fman/test/";
+    dirnowR="C:/"
+    showList(dirnowL);
+    // showList(dirnowR,"right");
 }
 
 document.onkeydown = function (event) {
     var e = event || window.event || arguments.callee.caller.arguments[0];
+    // when key "BackSpace" pressed
     if (e && e.keyCode == 8) { 
         // delete last folder
-        dirnow=document.getElementById("leftdir").innerHTML;
-        var dirbackarr=dirnow.split("/")
+        dirnowL=document.getElementById("leftdir").innerHTML;
+        var dirbackarr=dirnowL.split("/")
         dirbackarr.splice(-2,1)
-        dirback=dirbackarr.join('/');
+        var dirback=dirbackarr.join('/');
         console.log("back is "+dirback);
         showList(dirback);  
+        resetCursor();
+    }
+    // when key "up arrow" pressed
+    var filebtns=document.getElementsByClassName("name");
+    if (e && e.keyCode == 38) {
+        if(cursorPositionL==0)
+            cursorPositionL=filebtns.length-1;
+        else
+            cursorPositionL=cursorPositionL-1;
+        filebtns[cursorPositionL].click();
+    }
+    // when key "down arrow" pressed
+    if (e && e.keyCode == 40) {
+        if(cursorPositionL==filebtns.length-1)
+            cursorPositionL=0;
+        else
+            cursorPositionL=cursorPositionL+1;
+        filebtns[cursorPositionL].click();
+    }
+    // when "enter" pressed
+    if (e && e.keyCode == 13) {
+        //should be optimize with dblckick function
+        var filebtns=document.getElementsByClassName("name");
+        if(filebtns[cursorPositionL].classList[1]=="folder"){
+            // if folder, open folder and show file list in folder
+            console.log("this is dir="+dirnowL);
+            dirnowL=document.getElementById("leftdir").innerHTML;
+            showList(dirnowL+filebtns[cursorPositionL].innerText+"/"); 
+            resetCursor();               
+        } 
+        else{
+            dirnowL=document.getElementById("leftdir").innerHTML;
+            // if file, open file by default program of system
+            exec("start"+" "+dirnowL+"/"+filebtns[cursorPositionL].innerText);
+            console.log("this is file "+filebtns[cursorPositionL].id);
+        }
+    }
+    // when key "tap" pressed
+}
+
+// show the background of current cursor position 
+function cursorBG(cursorPosition,activePan,bg){
+    let fileid;
+    if(activePan=="left")
+        fileid="Lfile";
+    else
+        fileid="Rfile";
+    var filerow=document.getElementById(fileid+cursorPosition.toString());
+    filerow.parentNode.style.backgroundColor=bg;
+
+}
+// file click function , chang cursor position and change backgroundColor
+function fileclickfun(){
+    var filebtns=document.getElementsByClassName("name");
+    
+    for(let i=0; i<filebtns.length;i++){
+        
+        filebtns[i].addEventListener('click', function(){
+            if(filebtns[i].id.slice(0,5)=="Lfile"){
+                activePan="left";
+                cursorPositionL=i;
+            }
+            else{
+                activePan="right";
+                cursorPositionR=i;
+            }
+            clearBG();
+            cursorBG(i,activePan,"#49483e");
+        })
     }
 }
-// file click function
-function fileclickfun(){
+function clearBG(){
+    var filebtns=document.getElementsByClassName("name");
+    for(var j=0; j<filebtns.length;j++){
+        cursorBG(j,activePan,"#272822")
+    }
+}
+function resetCursor(){
+    if(activePan=="left"){
+        cursorPositionL=0;
+        cursorBG(cursorPositionL,activePan,"#49483e");
+    }
+    else{
+        cursorPositionR=0;
+        cursorBG(cursorPositionR,activePan,"#49483e");
+    }
+}
+// file double click function
+function filedblclickfun(){
     var filebtns=document.getElementsByClassName("name");
     for(let i=0; i<filebtns.length;i++){
       
         filebtns[i].addEventListener('dblclick', function(){
-            var btn=document.getElementById("leftfile"+i.toString());
+            var btn=document.getElementById("Lfile"+i.toString());
             
             if(btn.classList[1]=="folder"){
                 // if folder, open folder and show file list in folder
-                console.log("this is dir="+dirnow);
-                dirnow=document.getElementById("leftdir").innerHTML;
-                showList(dirnow+btn.innerText+"/");                
+                console.log("this is dir="+dirnowL);
+                
+                dirnowL=document.getElementById("leftdir").innerHTML;
+                showList(dirnowL+btn.innerText+"/");
+                resetCursor();                
             } 
             else{
-                dirnow=document.getElementById("leftdir").innerHTML;
+                dirnowL=document.getElementById("leftdir").innerHTML;
                 // if file, open file by default program of system
-                exec("start"+" "+dirnow+"/"+btn.innerText);
+                exec("start"+" "+dirnowL+"/"+btn.innerText);
                 console.log("this is file "+btn.id);
             }
-        })
+        });
     }
     console.log("click setted");
 }
 
+
 // input dir show filelist on windows
 function showList(directory){
-    dirnow=directory;
+    dirnowL=directory;
     fs.readdir(directory,function(err, files){
         if (err) {
             return console.error(err);
@@ -60,10 +154,13 @@ function showList(directory){
         tabelLeft.innerHTML=nameToHtmlTable(directory,files);
         sizeToHtml(directory,files);
 
-        // fileclickfun() should be in call back function
-        fileclickfun();
+        // filedblclickfun() should be in call back function
+        filedblclickfun();
+        cursorBG(cursorPositionL,activePan,"#49483e");
+        fileclickfun();        
     });    
     console.log("finished show list");
+    
     
 }
 
@@ -74,7 +171,7 @@ function nameToHtmlTable(directory,files){
     for(var i=0, k=0; i<files.length;i++){
         if(files[i][0]!="." && files[i][0]!="$"){
             console.log(files[i].toString())
-            var fileid="leftfile"+k.toString();
+            var fileid="Lfile"+k.toString();
             k++;
             tableString=tableString+"<tr>"+"<td class='name',"+" id="+ fileid+">"+files[i]+"</td></tr>";
                        
