93658ba6f3c810fbff2b62a4479004ca871a7505
Harvey <hw-li@foxmail.com>
2019-11-10 21:00:11 +0800

    main.js: update commit

--- main.js
+++ main.js
@@ -8,19 +8,15 @@ const fs = require("fs")
 
 var mainWindow = null;
 
-// 当所有窗口被关闭了，退出。
+// quit while closed
 app.on('window-all-closed', function() {
-  // 在 OS X 上，通常用户在明确地按下 Cmd + Q 之前
-  // 应用会保持活动状态
   if (process.platform != 'darwin') {
     app.quit();
   }
 });
 
-// 当 Electron 完成了初始化并且准备创建浏览器窗口的时候
-// 这个方法就被调用
+// eletron initializetion, create window
 app.on('ready', function() {
-  // 创建浏览器窗口。
   mainWindow = new BrowserWindow({
     width: 1400, 
     height: 800+39,
@@ -29,16 +25,13 @@ app.on('ready', function() {
     }
   });
   Menu.setApplicationMenu(null)
-  // 加载应用的 index.html
+// load index.html
   mainWindow.loadURL('file://' + __dirname + '/index.html');
-  // 打开开发工具
+// opem developmemt tools 
   mainWindow.openDevTools();
 
-  // 当 window 被关闭，这个事件会被发出
+//when window closed
   mainWindow.on('closed', function() {
-    // 取消引用 window 对象，如果你的应用支持多窗口的话，
-    // 通常会把多个 window 对象存放在一个数组里面，
-    // 但这次不是。
     mainWindow = null;
   });
 });
